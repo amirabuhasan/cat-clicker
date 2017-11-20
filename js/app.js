14b64c01562052e01c320dbcf48ef0c6909489f0
amirabuhasan <amirabuhasan@gmail.com>
2017-11-20 22:29:00 +0800

    Seperated view from viewmodel

--- js/app.js
+++ js/app.js
@@ -1,18 +1,45 @@
-let ViewModel = function() {
+let Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable("Tabby");
   this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
-  this.level = ko.observable("Newborn");
-
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
-
-  this.incrementLevel = ko.computed(function() {
-    if (this.clickCount() >= 10) {
-      return this.level("Infant")
+  this.nicknames = ko.observableArray([
+    {name: "Tabtab"},
+    {name: "T-Bone"},
+    {name: "Mr. T"},
+    {name:"Tabitha"}
+  ]);
+  this.level = ko.computed(function() {
+    let level;
+    if (this.clickCount() < 10) {
+      level = "Newborn";
     }
-  }, this)
+    if (this.clickCount() >= 10) {
+      level = "Infant";
+    }
+    if (this.clickCount() >= 20) {
+      level = "Child";
+    }
+    if (this.clickCount() >= 30) {
+      level = "Teen";
+    }
+    if (this.clickCount() >= 40) {
+      level = "Adult";
+    }
+    if (this.clickCount() >= 50) {
+      level = "Ninja";
+    }
+    return level;
+  }, this);
+}
+
+let ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
+  
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
 };
+
+
 
 ko.applyBindings(new ViewModel());
